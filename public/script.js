f159811a56f18ba26cd7bdd4194fe573d41e189c
Guilherme Cabrini <guilherme.silva@entelgy.com>
2017-08-20 10:52:28 -0300

    add lodash and formattedTotal computed data

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
-new Vue({
-    el: '#app'
+var app = new Vue({
+    el: '#app',
+    data: {
+        total: 0
+    },
+    computed: {
+        formattedTotal: function() {
+            return '$' + _.round2Fixed(this.total)
+        }
+    }
 });
