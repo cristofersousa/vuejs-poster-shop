db4a37766a303aea0f1bfa03525a5806fb219584
Guilherme Cabrini <guilherme.silva@entelgy.com>
2017-08-22 12:26:43 -0300

    add price and monetary filter

--- public/script.js
+++ public/script.js
@@ -3,39 +3,54 @@ var app = new Vue({
     data: {
         total: 0,
         items: [
-          { id: 1, title: 'Item 1'},
-          { id: 2, title: 'Item 2'},
-          { id: 3, title: 'Item 3'},
-          { id: 4, title: 'Item 4'},
-          { id: 5, title: 'Item 5'}
+          { id: 1, title: 'Item 1', price: 9.99 },
+          { id: 2, title: 'Item 2', price: 19.99 },
+          { id: 3, title: 'Item 3', price: 5.99 },
+          { id: 4, title: 'Item 4', price: 31.99 },
+          { id: 5, title: 'Item 5', price: 2.99 }
         ],
         cart: []
     },
-    computed: {
-        formattedTotal: function() {
-            return '$' + _.round2Fixed(this.total)
+    filters: {
+      monetaryValue: function(value, currencyType) {
+            switch (currencyType) {
+              case 'R$':
+                value = value * 3.15
+                break;
+              default:
+            }
+
+            return currencyType + _.round2Fixed(value)
         }
     },
     methods: {
-        addItem: function(index) {
-            this.total += 9.99;
+        addToCart: function(index) {
             var item = this.items[index];
             var cartItem = this.cart.filter(function(carItem) {
               return carItem.id === item.id;
             });
 
-            if (cartItem.length > 0) {
+            if (this.hasItems(cartItem)) {
               cartItem.shift().qty++;
             } else {
-              this.cart.push({
+              this.addItem(this.cart, {
                 id: item.id,
                 title: item.title,
+                price: item.price,
                 qty: 1
-              });
+              })
             }
+
+            this.updateTotal(item);
         },
-        hasCartItems: function() {
-          return this.cart.length > 0
+        addItem: function(list, item) {
+          list.push(item);
+        },
+        hasItems: function(list) {
+          return list.length > 0
+        },
+        updateTotal: function(item) {
+          this.total += item.price;
         }
     }
 });
