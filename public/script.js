1016a22901b6186aefca994038262a579bb49190
Guilherme Cabrini <guilherme.silva@entelgy.com>
2017-08-22 12:03:29 -0300

    add item cart quantity

--- public/script.js
+++ public/script.js
@@ -3,11 +3,11 @@ var app = new Vue({
     data: {
         total: 0,
         items: [
-          { title: 'Item 1'},
-          { title: 'Item 2'},
-          { title: 'Item 3'},
-          { title: 'Item 4'},
-          { title: 'Item 5'}
+          { id: 1, title: 'Item 1'},
+          { id: 2, title: 'Item 2'},
+          { id: 3, title: 'Item 3'},
+          { id: 4, title: 'Item 4'},
+          { id: 5, title: 'Item 5'}
         ],
         cart: []
     },
@@ -18,7 +18,24 @@ var app = new Vue({
     },
     methods: {
         addItem: function(index) {
-            console.log(index)
+            this.total += 9.99;
+            var item = this.items[index];
+            var cartItem = this.cart.filter(function(carItem) {
+              return carItem.id === item.id;
+            });
+
+            if (cartItem.length > 0) {
+              cartItem.shift().qty++;
+            } else {
+              this.cart.push({
+                id: item.id,
+                title: item.title,
+                qty: 1
+              });
+            }
+        },
+        hasCartItems: function() {
+          return this.cart.length > 0
         }
     }
 });
